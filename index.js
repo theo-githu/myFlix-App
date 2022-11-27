836d1fdf0e5cfe356f7b7c05a8c31af46a24e4b1
theo-githu <111968864+theo-githu@users.noreply.github.com>
2022-11-27 10:24:10 +0000

    Update index.js
    require('cors')

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ mongoose.connect('mongodb://localhost:27017/test',
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const cors = require('cors');
+app.use((cors));
+
 let auth = require('./auth')(app);
 
 const passport = require('passport');
